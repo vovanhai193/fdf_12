757b40fa8fd6c40d580d08f7e2024da6b5fef5ea
MinhDuc <duc11t3bk@gmail.com>
2016-09-16 17:20:22 +0700

    admin_approve_reject_shop_request

--- app/controllers/admin/shop_requests_controller.rb
+++ app/controllers/admin/shop_requests_controller.rb
@@ -1,10 +1,37 @@
-class Admin::ShopRequestsController < ApplicationController
-  before_action :authenticate_admin!
+class Admin::ShopRequestsController < AdminController
+  load_and_authorize_resource class: "Shop"
 
   def index
     @shops = Shop.pending.page(params[:page]).per(Settings.common.per_page)
   end
 
   def update
+    if @shop_request.update_attributes(shop_request_params)
+      respond_to do |format|
+        format.json do
+          render json: {
+            is_success: true,
+            message: t("admin.shop_requests.index.update_shop_request_success",
+              shop: @shop_request.name, status: @shop_request.status)
+          }
+        end
+      end
+    else
+      respond_to do |format|
+        format.json do
+          render json: {
+            is_success: false,
+            message: t("admin.shop_requests.index.update_shop_request_failed",
+              shop: @shop_request.name, status: @shop_request.status)
+          }
+        end
+      end
+    end
+  end
+
+  private
+  def shop_request_params
+    params[:shop_request][:status] = params[:shop_request][:status].to_i
+    params.require(:shop_request).permit :status
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
   attr_accessor :owner_id
+  after_create :create_shop_manager
   acts_as_paranoid
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
   has_many :reviews, as: :reviewable
@@ -9,9 +10,7 @@ class Shop < ApplicationRecord
   has_many :orders
   has_many :order_products, through: :orders
 
-  after_create :create_shop_manager
-
-  enum status: {pending: 0, active: 1, closed: 2, blocked: 3}
+  enum status: {pending: 0, active: 1, closed: 2, rejected: 3, blocked: 4}
 
   validates :name, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {maximum: 50}
@@ -34,4 +33,5 @@ class Shop < ApplicationRecord
       errors.add(:avatar, I18n.t("pictures.error_message", max_size: max_size))
     end
   end
+
 end
