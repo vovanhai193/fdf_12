c62a8e8a268ff08d680f4e9a6a4e2e602c5709d4
hoangmirs <hoang.mirs@gmail.com>
2016-09-16 13:50:38 +0700

    Admin update/delete category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,6 +18,32 @@ class Admin::CategoriesController < AdminController
     @categories = @categories.page(params[:page]).per Settings.common.per_page
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t "flash.success.admin.updated_category"
+      redirect_to admin_categories_path
+    else
+      flash[:danger] = t "flash.danger.admin.updated_category"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category.products.any?
+      flash[:warning] = t "flash.warning.admin.deleted_category"
+    else
+      if @category.destroy
+        flash[:success] = t "flash.success.admin.deleted_category"
+      else
+        flash[:danger] = t "flash.danger.admin.deleted_category"
+      end
+    end
+    redirect_to admin_categories_path
+  end
+
   private
   def category_params
     params.require(:category).permit :name
