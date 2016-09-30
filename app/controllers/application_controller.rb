423a63d09cdc4109a57ea3e1362832decff7fcc2
trantrinh1102 <trantrinh.1102@gmail.com>
2016-09-30 16:56:28 +0700

    Add more field signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
       user_params.permit :name, :email, :chatwork_id, :avatar, :description,
         :password, :password_confirmation, :current_password
     end
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit :name, :email, :chatwork_id, :avatar, :description,
+        :password, :password_confirmation
+    end
   end
 
   private
