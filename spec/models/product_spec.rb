ecbd6ad340c0ab2b872b80e663bff24ba74419e6
phamxuanvu <xuanvupham96@gmail.com>
2016-10-28 09:13:34 +0700

    Test product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,34 @@
 require "rails_helper"
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:product) {FactoryGirl.create(:product)}
+   subject {product}
+
+  context "scope" do
+    it {expect(Product.top_products).to eq([subject])}
+  end
+
+  context "Associations" do
+    it {is_expected.to belong_to :category}
+    it {is_expected.to belong_to :shop}
+    it {is_expected.to belong_to :user}
+  end
+
+  describe "validates" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    context "validates name" do
+      before {subject.name = ""}
+      it "is not valid without a name" do
+        is_expected.to_not be_valid
+      end
+    end
+    context "validates description" do
+      before {subject.description = ""}
+      it "is not valid without a description" do
+        is_expected.to_not be_valid
+      end
+    end
+  end
 end
